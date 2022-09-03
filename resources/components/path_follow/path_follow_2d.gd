6bf1b77f26ac853d13bb0c2cdc4f67c0039f5795
1Luc1 <lukas.aigner@gmail.com>
2022-09-03 12:59:47 +0200

    changes for Godot_v4.0-alpha15

--- resources/components/path_follow/path_follow_2d.gd
+++ resources/components/path_follow/path_follow_2d.gd
@@ -15,18 +15,18 @@ func idle():
 func _process(delta):
 	if !_idle:
 		if backward:
-			offset -= 250 * delta
-			if unit_offset == 0:
+			progress -= 250 * delta
+			if progress_ratio == 0:
 				idle()
 		else:
-			offset += 250 * delta
-			if unit_offset == 1:
+			progress += 250 * delta
+			if progress_ratio == 1:
 				idle()
 
 func _on_map_path_entered(moving_direction, direction):
 	backward = moving_direction
 	if backward:
-		unit_offset = 1
+		progress_ratio = 1
 	else:
-		unit_offset = 0
+		progress_ratio = 0
 	move(direction)
